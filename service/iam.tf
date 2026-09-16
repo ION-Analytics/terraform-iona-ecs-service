dd51b6b73a9520467db736bbab398d3878f267b7
Paul Demers <paul.demers@iongroup.com>
2026-09-16 08:05:53 -0700

    Add EFS permissions/

--- service/iam.tf
+++ service/iam.tf
@@ -1,3 +1,33 @@
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "policy_document" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:Describe*",
+      "elasticfilesystem:Describe*",
+      "elasticfilesystem:ClientMount",
+      "elasticfilesystem:List*",
+      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+      "elasticloadbalancing:DeregisterTargets",
+      "elasticloadbalancing:Describe*",
+      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+      "elasticloadbalancing:RegisterTargets",
+    ]
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_role" "role" {
   name_prefix = join(
     "",
@@ -9,18 +39,7 @@ resource "aws_iam_role" "role" {
   )
 
   # from http://docs.aws.amazon.com/AmazonECS/latest/developerguide/service_IAM_role.html
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Principal = { 
-          Service =  "ecs.amazonaws.com" 
-        },
-        Effect = "Allow"
-      }
-    ]
-  })
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
 resource "aws_iam_role_policy" "policy" {
@@ -35,23 +54,7 @@ resource "aws_iam_role_policy" "policy" {
   )
 
   # from http://docs.aws.amazon.com/AmazonECS/latest/developerguide/service_IAM_role.html (step 7)
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "ec2:AuthorizeSecurityGroupIngress",
-          "ec2:Describe*",
-          "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-          "elasticloadbalancing:DeregisterTargets",
-          "elasticloadbalancing:Describe*",
-          "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-          "elasticloadbalancing:RegisterTargets",
-        ],
-        Resource = "*",
-      }
-    ]
-  })
+  policy = data.aws_iam_policy_document.policy_document.json
+
 }
 
